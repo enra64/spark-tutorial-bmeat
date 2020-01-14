1913ce46f165964576630a26d02cbffbfa3cdff7
enra64 <sujomear@arcor.de>
2020-01-14 16:49:25 +0100

    first working changes

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -4,8 +4,14 @@ import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 object Sindy {
 
-  def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-    // TODO
+  def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    inputs.foreach(input => spark.read
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .option("delimiter", ";")
+      .csv(input)
+      .printSchema())
+
   }
 }
